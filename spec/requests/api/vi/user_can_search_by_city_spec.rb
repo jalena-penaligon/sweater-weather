452690e671f70fe447ba9095702b82888de3f3ae
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-06-01 15:46:13 -0600

    Adds test to asert that date and time of search appears

--- spec/requests/api/vi/user_can_search_by_city_spec.rb
+++ spec/requests/api/vi/user_can_search_by_city_spec.rb
@@ -7,17 +7,22 @@ describe 'Weather API' do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+    data = JSON.parse(response.body)["data"]
 
-    city = JSON.parse(response.body)["data"]
- end
+    expect(data["attributes"]["city_state"]).to eq("Denver, CO")
+    expect(data["attributes"]["country"]).to eq("United States")
+  end
 
- it 'displays the date & time of the search' do
-   location = Location.create(city_state: "Denver, CO", country: "United States", lat: "39.7392358", long: "-104.990251")
+  it 'displays the date & time of the search' do
+    location = Location.create(city_state: "Denver, CO", country: "United States", lat: "39.7392358", long: "-104.990251")
 
-   get '/api/v1/forecast?location=denver,co'
+    get '/api/v1/forecast?location=denver,co'
 
-   expect(response).to be_successful
+    expect(response).to be_successful
 
-   city = JSON.parse(response.body)["data"]
-end
+    data = JSON.parse(response.body)["data"]
+
+    expect(data["attributes"]["search_time"]).to eq(Time.now.strftime("%l:%M %p"))
+    expect(data["attributes"]["search_date"]).to eq("#{Time.now.strftime("%m")}/#{Time.now.strftime("%d")}")
+  end
 end

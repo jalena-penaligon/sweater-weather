74ea64174c70327a7a70b5f2d33662fcc1125e04
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-06-03 10:33:14 -0600

    Changes folder name from vi to v1

--- spec/requests/api/v1/user_can_find_a_citys_antipode_spec.rb
+++ spec/requests/api/v1/user_can_find_a_citys_antipode_spec.rb
@@ -4,7 +4,7 @@ describe 'Antipode API' do
   it 'displays a citys antipode and its weather' do
     location = Location.create(city_state: "Sheung Wan, Hong Kong", country: "China", lat: "22.2863943", long: "114.1491375", background_image: "https://cdn.pixabay.com/photo/2017/07/19/01/41/clouds-2517653_1280.jpg")
 
-    get '/api/v1/antipode?loc=hongkong'
+    get '/api/v1/antipode?loc=hong kong,cn'
 
     expect(response).to be_successful
     data = JSON.parse(response.body)

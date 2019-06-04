38c0990863011d7c66595988173d0eea006571e7
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-06-04 11:35:05 -0600

    Completes functionality for a user to login via post request

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
-    binding.pry
-    user = User.find_by(email: params[:email]
-    if user.valid_password?(params[:password])
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
+      render status: 200,
+      json: {
+        body: {
+        api_key: user.access_token
+        }
+      }
+    end
   end
-
-  def
-
 end
--- spec/requests/api/v1/user_can_login_spec.rb
+++ spec/requests/api/v1/user_can_login_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 describe 'Login API' do
   it 'can create a new session', type: :feature do
+    user = User.create(email: "whatever@example.com", password: "password", password_confirmation: "password", access_token: SecureRandom.uuid)
     user_params = {
       "email": "whatever@example.com",
-      "password": "password",
+      "password": "password"
     }
 
     page.driver.post('/api/v1/sessions', user_params )
     expect(page.driver.status_code).to eq(200)
-
-    user = User.last
-    expect(user.email).to eq(user_params[:email])
+    expect(page).to have_content(user.access_token)
   end
 end
